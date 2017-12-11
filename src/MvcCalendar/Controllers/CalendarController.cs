ce9508d1b8ba70b1b260ce2bbf1a592333c9b20f
Humanity's last hope <eridolfi@pbi.org>
2017-12-11 16:30:31 -0500

    Added json

--- src/MvcCalendar/Controllers/CalendarController.cs
+++ src/MvcCalendar/Controllers/CalendarController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcCalendar.Models;
+using System.Text;
 
 namespace MvcCalendar.Controllers
 {
@@ -15,6 +16,25 @@ namespace MvcCalendar.Controllers
             var course = new Courses();
 
             return View();
+        }
+
+        public ActionResult TestJSON()
+        {
+            string text = "";
+            try
+            {
+                using (var webClient = new System.Net.WebClient())
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    var json2 = webClient.DownloadString("http://manage.hawksearch.com/sites/pbi/?&mpp=200&hawkoutput=custom&hawkitemlist=json");
+                    return Json(json2, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                text = "error";
+            }
+            return Json(new { json = text });
         }
     }
 }
